69b651174566654d503916980bb06f05d71bf5fa
Han Jing <hanqijing@gmail.com>
2012-04-13 03:30:02 -0700

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+end

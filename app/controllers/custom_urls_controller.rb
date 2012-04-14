ec32a476a397e56215435b294dcfee131b5bba88
Han Jing <hanqijing@gmail.com>
2012-04-14 03:44:08 -0700

    added missing http beginning handling

--- app/controllers/custom_urls_controller.rb
+++ app/controllers/custom_urls_controller.rb
@@ -4,7 +4,11 @@ class CustomUrlsController < ApplicationController
   end
 
 	def create
+		@url = validate_redirect_url(params[:custom_urls][:url])
+		params[:custom_urls][:url] = @url
+		puts @url
 		@custom_url = CustomUrl.new(params[:custom_urls])
+
 
 		if @custom_url.save
 			redirect_to custom_urls_path
@@ -27,5 +31,13 @@ class CustomUrlsController < ApplicationController
 		@custom_url = CustomUrl.find(params[:id])
 		@custom_url.delete
 		redirect_to custom_urls_path
+	end
+
+	def validate_redirect_url(str)
+		if str[0..6] == 'http://' or str[0..7] == 'https://'
+			return str
+		else
+			return 'http://' + str
+		end
 	end
 end

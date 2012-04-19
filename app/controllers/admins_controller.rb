e19ce59b4ecde1c3c032c3efc788730687ad6243
Han Jing <hanqijing@gmail.com>
2012-04-19 01:10:28 -0700

    fixed routing for logging in

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,12 +2,16 @@ class AdminsController < ApplicationController
 	def manage_urls
 		if current_admin
 			redirect_to custom_urls_path
+		else
+			redirect_to new_admin_session_path
 		end
 	end
 
 	def manage_subs
 		if current_admin
 			@subscribers = Subscriber.all
+		else
+			redirect_to new_admin_session_path
 		end
 	end
 end

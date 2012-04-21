5d6d3e4eae6c7daa6b14537f53b4d19628287212
Han Jing <hanqijing@gmail.com>
2012-04-21 03:25:34 -0700

    fixed javascript error on front page

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,14 +1,13 @@
 class SubscribersController < ApplicationController
 	
 	
-	before_filter :require_login
   def create
     @subscriber = Subscriber.new(params[:subscriber])
     @success = false
 
     if @subscriber.save
 			#SubMailer.welcome_email(@subscriber).deliver
-			SubMailer.sub_email(@subscriber).deliver
+			#SubMailer.sub_email(@subscriber).deliver
 			puts 'We saved the subscriber!'
       @success = true
       @msg = 'Thank you for signing up! You will be the first to know when we launch.'
@@ -28,24 +27,6 @@ class SubscribersController < ApplicationController
       end 
     end 
   end 
-
-  private
- 
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_admin_session_path
-    end
-  end
- 
-  # The logged_in? method simply returns true if the user is logged
-  # in and false otherwise. It does this by "booleanizing" the
-  # current_user method we created previously using a double ! operator.
-  # Note that this is not common in Ruby and is discouraged unless you
-  # really mean to convert something into true or false.
-  def logged_in?
-    !!current_admin
-  end
 
 end
 
